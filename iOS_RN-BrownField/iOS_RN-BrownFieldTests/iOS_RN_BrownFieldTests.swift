8ceb577503c7586b002bb539c4ef6af14938e739
Changhyun Kang <changhyun.kyle@gmail.com>
2024-12-14 20:30:47 +0900

    config: 프로젝트 초기 설정

---
+++ iOS_RN-BrownField/iOS_RN-BrownFieldTests/iOS_RN_BrownFieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_RN_BrownFieldTests.swift
+//  iOS_RN-BrownFieldTests
+//
+//  Created by 강창현 on 12/14/24.
+//
+
+import Testing
+@testable import iOS_RN_BrownField
+
+struct iOS_RN_BrownFieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
